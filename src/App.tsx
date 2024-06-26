5200826c1330aefdf86d4dfb3dfc9adaab7e9b4b
prashikwagh <prashik.wagh22@vit.edu>
2024-06-27 01:55:10 +0530

    Update App.tsx to fetch data continuously and show graph

--- src/App.tsx
+++ src/App.tsx
@@ -4,44 +4,63 @@ import Graph from './Graph';
 import './App.css';
 
 /**
- * State declaration for <App />
+
  */
 interface IState {
-  data: ServerRespond[],
+  data: DataPoint[];
+  showGraph: boolean;  // Add this line
 }
+
 
 /**
  * The parent element of the react app.
  * It renders title, button and Graph react element.
  */
-class App extends Component<{}, IState> {
+class App extends React.Component<{}, IState> {
   constructor(props: {}) {
     super(props);
-
     this.state = {
-      // data saves the server responds.
-      // We use this state to parse data down to the child element (Graph) as element property
       data: [],
+      showGraph: false  // Initialize showGraph
     };
   }
+  // Other constructor code...
+}
+
 
   /**
    * Render Graph react component with state.data parse as property data
    */
-  renderGraph() {
-    return (<Graph data={this.state.data}/>)
+renderGraph() {
+  if (this.state.showGraph) {
+    return (
+      <div className="graph-container">
+        <Graph data={this.state.data} />
+      </div>
+    );
+  } else {
+    return null;  // Render nothing if showGraph is false
   }
+}
+
 
   /**
    * Get new data from server and update the state with the new data
    */
-  getDataFromServer() {
-    DataStreamer.getData((serverResponds: ServerRespond[]) => {
-      // Update the state by creating a new array of data that consists of
-      // Previous data in the state and the new data from server
-      this.setState({ data: [...this.state.data, ...serverResponds] });
-    });
-  }
+getDataFromServer() {
+  setInterval(() => {
+    fetch('server-url')  // Replace 'server-url' with your actual server endpoint
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          data: data,
+          showGraph: true  // Show graph once data is fetched
+        });
+      })
+      .catch(error => console.error('Error fetching data:', error));
+  }, 5000);  // Fetch data every 5 seconds (adjust as needed)
+}
+
 
   /**
    * Render the App react component
